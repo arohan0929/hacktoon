166767678c8fd6b6cef9df2ce9196e5cb3130535
arohan0929 <arohan0929@gmail.com>
2022-09-17 11:12:07 -0400

    initial setup

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+
+
+app.use(express.static("public"));
+
+
+
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res)=>{
+   res.render("index");
+});
+
+app.listen(3000);
